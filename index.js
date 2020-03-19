d56acf7b6677023c3936f2772d7045fb80f38174
thedanielenrico <thedanielenrico@gmail.com>
2020-03-18 21:16:49 -0700

    finish project

--- index.js
+++ index.js
@@ -3,22 +3,53 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)')
 const count = document.getElementById('count')
 const total = document.getElementById('total')
 const movieSelect = document.getElementById('movie')
+populateUI()
 
 let ticketPrice = +movieSelect.value
+
+// Save selected movie index and price 
+
+const setMovieData = (movieIndex, moviePrice) => {
+    localStorage.setItem('selectedMovieIndex', movieIndex)
+    localStorage.setItem('selectedMoviePrice', moviePrice)
+}
 
 // Update total and count
 const updateSelectedCount = () => {
     const selectedSeats = document.querySelectorAll('.row .seat.selected')
     const selectedSeatsCount = selectedSeats.length
 
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat))
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
+
     count.innerText = selectedSeatsCount
     total.innerText = selectedSeatsCount * ticketPrice
 }
 
-// Movie select event
+// Get data from local storage and populate UI
 
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'))
+
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected')
+            }
+        })
+    }
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex')
+
+    if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex
+    }
+}
+
+// Movie select event
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value
+    setMovieData(e.target.selectedIndex, e.target.value)
     updateSelectedCount()
 })
 
@@ -30,3 +61,5 @@ container.addEventListener('click', (e) => {
         updateSelectedCount()
     }
 })
+
+updateSelectedCount()
